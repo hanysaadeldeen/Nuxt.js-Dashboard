8b44cc6b745bbc170b0df520b60ad8e0d25241f5
HanyMohamed <hsaad9231@gmail.com>
2024-11-09 23:10:36 +0200

    update nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,34 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// // https://nuxt.com/docs/api/configuration/nuxt-config
+// export default defineNuxtConfig({
+//   compatibilityDate: "2024-04-03",
+//   devtools: { enabled: true },
+//   css: [
+//     "bootstrap/dist/css/bootstrap.min.css",
+//     "bootstrap-icons/font/bootstrap-icons.css",
+//   ],
+//   plugins: ["~/plugins/supabase.js"],
+//   modules: [
+//     "@vee-validate/nuxt",
+//     "@element-plus/nuxt",
+//     "nuxt-graphql-client",
+//     "@nuxt/image",
+//   ],
+
+//   veeValidate: {
+//     autoImports: true,
+//   },
+
+//   build: {
+//     transpile: [],
+//   },
+//   runtimeConfig: {
+//     public: {
+//       GQL_HOST: process.env.GQL_HOST,
+//       supabaseUrl: process.env.NUXT_SUPABASE_URL,
+//       supabaseKey: process.env.NUXT_SUPABASE_KEY,
+//     },
+//   },
+// });
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
@@ -21,11 +51,18 @@ export default defineNuxtConfig({
   build: {
     transpile: [],
   },
+
   runtimeConfig: {
     public: {
       GQL_HOST: process.env.GQL_HOST,
       supabaseUrl: process.env.NUXT_SUPABASE_URL,
       supabaseKey: process.env.NUXT_SUPABASE_KEY,
     },
+  },
+
+  ssr: true, // تمكين الـ SSR
+
+  app: {
+    baseURL: "/", // ضبط قاعدة التوجيه بشكل صحيح
   },
 });
